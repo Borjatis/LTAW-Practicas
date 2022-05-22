56e77f69342083b312598a8da0d3336165044f78
Borjatis <borja.ca@gmail.com>
2022-05-22 20:14:25 +0200

    Servidor web de la tienda

---
+++ P1/Tienda/tienda.js
@@ -0,0 +1,80 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const ls_OUT = "ls.html"
+
+
+const PUERTO = 9090;
+
+const mime = {
+    'html' : 'text/html',
+    'css'  : 'text/css',
+    'jpg'  : 'image/jpg',
+    'ico'  : 'image/x-icon'
+ };
+
+const server = http.createServer((req, res)=>{
+    console.log("Petición recibida!");
+
+    //-- Analizar el recurso
+    //-- Construir el objeto url con la url de la solicitud
+
+    const objecturl = url.parse(req.url);
+    console.log(url.pathname);
+    let camino='pages and images'+objecturl.pathname;
+    //-- En el caso de url vacía, devolvemos main.
+    if (camino=='pages and images/')
+      camino='pages and images/main.html';
+    fs.stat(camino, error => {
+
+      if (!error) {
+
+        fs.readFile(camino, (error,contenido) => {
+          if (error) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.write('Error interno');
+            res.end();					
+          } else {           
+            const vec = camino.split('.');
+            const extension=vec[vec.length-1];
+            const mimearchivo=mime[extension];
+            res.writeHead(200, {'Content-Type': 'mimearchivo'});
+            res.write(contenido);
+            res.end();
+          }
+        });
+      } else {
+        console.log(camino);
+          if(camino == 'pages and images/ls'){
+            fs.readdir("./pages and images", (error, files) => { 
+              if(error){
+                  throw error
+              }
+              console.log("Finalizando lectura");
+              console.log(files);
+            file=(files.toString());
+              fs.writeFileSync(ls_OUT, file);
+              res.writeHead(200, {'Content-Type': 'html'});
+              res.write(file);
+            res.end();
+          });
+          console.log("iniciando lectura");
+            
+          }else{
+            console.log("Recurso inexistente");
+            fs.readFile('pages and images/fail.html',(error,contenido) => {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write(contenido);    
+            return res.end();
+          });
+        }
+      }
+      
+    });
+
+});
+
+server.listen(PUERTO);
+
+console.log("Server On. Listening in port: " + PUERTO);
